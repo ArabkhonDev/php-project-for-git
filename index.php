ca672e14c73248afe6e3a6fa182c8992970fa488
ArabkhonDev <rightsolutionhub@gmail.com>
2025-02-01 10:02:39 +0500

    change index.php

--- index.php
+++ index.php
@@ -10,6 +10,16 @@
     <title>Document</title>
 </head>
 <body>
-    
+    <main>
+        <nav>
+            <link rel="stylesheet" href=""><a href="">Lorem.</a></link>
+            <link rel="stylesheet" href=""><a href="">Quo.</a></link>
+            <link rel="stylesheet" href=""><a href="">Possimus?</a></link>
+            <link rel="stylesheet" href=""><a href="">Fuga.</a></link>
+        </nav>
+    </main>
+    <footer>
+        bu footer
+    </footer>
 </body>
 </html>
